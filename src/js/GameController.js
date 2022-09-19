577ff2092302a63cbc06e9433e14563ef60bd8f0
Nicko Sinnikov <nickosinnikov@outlook.com>
2022-09-19 23:18:57 +0300

    first commit

---
+++ src/js/GameController.js
@@ -0,0 +1,26 @@
+import GamePlay from "./GamePlay";
+import themes from "./themes";
+export default class GameController {
+    constructor(gamePlay, stateService) {
+        this.gamePlay = gamePlay;
+        this.stateService = stateService;
+    }
+
+    init() {
+        // TODO: add event listeners to gamePlay events
+        this.gamePlay.drawUi(themes[1]); //themes[gameState.level];
+        // TODO: load saved stated from stateService
+    }
+
+    onCellClick(index) {
+        // TODO: react to click
+    }
+
+    onCellEnter(index) {
+        // TODO: react to mouse enter
+    }
+
+    onCellLeave(index) {
+        // TODO: react to mouse leave
+    }
+}
